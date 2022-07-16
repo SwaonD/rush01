7d304520f8aa50370ac6bba195022f5870f1c18f
Ethan MIS <emis@e3r4p5.clusters.42paris.fr>
2022-07-16 17:01:23 +0200

    Init index and such

--- ex00/main.c
+++ ex00/main.c
@@ -15,70 +15,35 @@
 #include <unistd.h>
 #include <stdio.h>
 
-int	*ft_init_parambuff(int *tab, int size)
-{
-	int	i;
 
-	i = 0;
-	tab = malloc(sizeof(int) * size);
-	while (i < size)
-	{
-		tab[i] = 0;
-		i++;
-	}
-	return (tab);
-}
+
 
 int	main(int argc, char **argv)
 {
 	int	i;
 	int	**tab;
+	int	**indexus;
 	int	size;
-	int	*col_up;
-	int	*col_do;
-	int	*row_le;
-	int	*row_ri;
-	// if (argc != 2)
-	// {
-	// 	write(1, "Error\n", 6);
-	// 	return (0);
-	// }
-	// i = -1;
-	// while (argv[1][i])
-	// 	i++;
-	// if (++i % 8 != 0 || i < 16 || i > 72)
-	// {
-	// 	write(1, "Error\n", 6);
-	// 	return (0);
-	// }
-	// size = i / 8;
-	// ft_init_parambuff(col_up, size);
-	// ft_init_parambuff(col_do, size);
-	// ft_init_parambuff(row_le, size);
-	// ft_init_parambuff(row_ri, size);
-	// i = -1;
-	// while (++i < size * 8)
-	// {
-	// 	if (i % 2 == 0)
-	// 	{
-	// 		if (i / (size * 2) == 0)
-	// 			col_up[i % (size * 2) / 2] = argv[1][i] - '0';
-	// 		if (i / (size * 2) == 1)
-	// 			col_do[i % (size * 2) / 2] = argv[1][i] - '0';
-	// 		if (i / (size * 2) == 2)
-	// 			row_le[i % (size * 2) / 2] = argv[1][i] - '0';
-	// 		if (i / (size * 2) == 3)
-	// 			row_ri[i % (size * 2) / 2] = argv[1][i] - '0';
-	// 		if (argv[1][i] > size + '0' || argv[1][i] < '0')
-	// 		{
-	// 			write(1, "Error\n", 6);
-	// 			return (0);
-	// 		}
-	// 	}
-	// }
+	if (argc != 2)
+	{
+		write(1, "Error\n", 6);
+		return (0);
+	}
+	i = -1;
+	while (argv[1][i])
+		i++;
+	if (++i % 8 != 0 || i < 16 || i > 72)
+	{
+		write(1, "Error\n", 6);
+		return (0);
+	}
+	size = i / 8;
 	size = 4;
-	tab = ft_init_tab(tab, size);
-	printf("%d\n", ft_is_valid(tab, 0, size));
+	tab = ft_init_tab(tab, size, size);
+	indexus = ft_init_index(argv[1], size);
+	if (indexus == NULL)
+		return(0);
+	printf("%d\n", ft_is_valid(tab, 0, size, indexus));
 	ft_print_tab(tab, size);
 	ft_free_tab(tab, size);
 
--- ex00/setup.c
+++ ex00/setup.c
@@ -18,14 +18,14 @@ void	ft_putchar(char c)
 	write(1, &c, 1);
 }
 
-int	**ft_init_tab(int **tab, int size)
+int	**ft_init_tab(int **tab, int rows, int size)
 {
 	int	i;
 	int	y;
 
 	i = 0;
 	tab = malloc(size * sizeof(int*));
-	while (i < size)
+	while (i < rows)
 	{
 		tab[i] = malloc(size * sizeof(int));
 		y = 0;
@@ -37,6 +37,28 @@ int	**ft_init_tab(int **tab, int size)
 		i++;
 	}
 	return (tab);
+}
+
+int	**ft_init_index(char *arg1, int size)
+{
+	int	i;
+	int	**indexus;
+
+	i = -1;
+	indexus = ft_init_tab(indexus, 4, size)
+	while (++i < size * 8)
+	{
+		if (i % 2 == 0)
+		{
+			if (arg1[i] > size + '0' || arg1[i] < '0')
+			{
+				write(1, "Error\n", 6);
+				return (NULL);
+			}
+			indexus[i / (size * 2)][i % (size * 2) / 2] = arg1[i] - '0';
+		}
+	}
+	return (indexus);
 }
 
 void	ft_print_tab(int **tab, int size)
