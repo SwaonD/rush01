1c76d82b3cd6e6ccb0d31ed74fca23453e6e911d
Ethan MIS <emis@e3r4p5.clusters.42paris.fr>
2022-07-16 11:03:57 +0200

    Les col et row, lets go

--- ex00/main.c
+++ ex00/main.c
@@ -12,14 +12,47 @@
 
 #include "header.h"
 
-int     main(int argc, char **argv)
+int	main(int argc, char **argv)
 {
-        int     **tab;
-        int     size;
+	int	col_up[4];
+	int	col_do[4];
+	int	row_le[4];
+	int	rox_ri[4];
+	int	i;
+	int	**tab;
+	int	size;
+	if (argc != 2)
+	{
+		write(1, "Error\n", 6);
+		return (0);
+	}
+	i = -1;
+	while (++i < 32)
+	{
+		if (i % 2 == 0)
+		{
+			if (i / 8 == 0)
+				col_up[i % 8 / 2] = argv[1][i] - '0';
+			if (i / 8 == 1)
+				col_do[i % 8 / 2] = argv[1][i] - '0';
+			if (i / 8 == 2)
+				row_le[i % 8 / 2] = argv[1][i] - '0';
+			if (i / 8 == 3)
+				row_ri[i % 8 / 2] = argv[1][i] - '0';
+			if (argv[1][i] > '4' || argv[1][i] < '0')
+			{
+				write(1, "Error\n", 6);
+				return (0);
+			}
+		}
+	}
+	size = 4;
+	tab = ft_init_tab(tab, size);
+	ft_edit_test_tab(tab, size);
+	ft_print_tab(tab, size);
+	ft_free_tab(tab, size);
 
-        size = 4;
-        tab = ft_init_tab(tab, size);
-        ft_edit_test_tab(tab, size);
-        ft_print_tab(tab, size);
-        ft_free_tab(tab, size);
+
+	write(1, "Error\n", 6);
+	return (0);
 }
